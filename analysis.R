c6bd5059cf43a85db45403c6386455307bdcc4ab
BMasinde <masindeb@live.com>
2018-10-17 22:28:24 +0200

    analysis 2.1

---
+++ analysis.R
@@ -0,0 +1,7 @@
+In 1965 USA was the largest consumer of oil and coal. 
+As of 2009 it was the largest consumer of oil at over 800 million tones.  
+China had a steady increase in coal consumption from 1965 to 1996 and a decline in the years 1997, 
+1998 and 1991. It experienced a rapid increase afterwards to become the largest consumer of coal
+at over 1500 million tonnes, nearly twice as much as USA. UK is the least consumer of oil and coal in this
+case. Brazil maintained the lowest consumption of coal, while France reduced both coal and oil consumption. 
+India has gradual increase in both consumption over the years.
